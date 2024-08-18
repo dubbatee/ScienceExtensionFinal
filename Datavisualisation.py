de6d8a41d1964c0850d454350ad2b17685b7546a
dubbatee <157960144+dubbatee@users.noreply.github.com>
2024-08-19 00:24:20 +1000

    Update Datavisualisation.py

--- Datavisualisation.py
+++ Datavisualisation.py
@@ -9,6 +9,7 @@ from scipy import stats
 from dsdistances import FundamentalblgdsF, FirstOvertoneblgdsF, FundamentaldiskdsF, FirstOvertonediskdsF
 from cephdistances import FundamentalblgcephF, FirstOvertoneblgcephF, FundamentaldiskcephF, FirstOvertonediskcephF
 
+### Data Visualisation Functions ###
 
 def modelvstrue(x1a, y1a, y1b, title="", xlabel="", ylabel="", colour1='#1984c5', colour2='#76c68f', marker1='.', marker2='.'):
 
@@ -16,21 +17,21 @@ def modelvstrue(x1a, y1a, y1b, title="", xlabel="", ylabel="", colour1='#1984c5'
     plt.scatter(x1a, y1b, c=colour2, marker=marker2, label="LMC Model", alpha=1)
     plt.xlabel('True Distance')
     plt.ylabel('Model Distances')
-    plt.title('Model Distances vs True Distances') 
+    plt.title(title)
     plt.legend()
     plt.show()
 
 
-def accfunction(x2a, y2a, y2b, title="", xlabel="", ylabel="", colour1='#1984c5', colour2='#76c68f', marker1='.', marker2='.'):
+def errorfunction(x2a, y2a, y2b, title="", xlabel="", ylabel="", colour1='#1984c5', colour2='#76c68f', marker1='.', marker2='.'):
     a, b = np.polyfit(x2a, y2a, 1)
     c, d = np.polyfit(x2a, y2b, 1)
     LOBF = [i * a + b for i in x2a]
     LOBF2 = [i * c + d for i in x2a]
     plt.scatter(x2a, y2a, c=colour1, marker=marker1, label="SMC Model", alpha=1)
     plt.scatter(x2a, y2b, c=colour2, marker=marker2, label="LMC Model", alpha=1)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title('Modeldist/Truedist')
+    plt.xlabel('True Distance')
+    plt.ylabel('Error%')
+    plt.title(title)
     plt.legend()
     plt.plot(x2a, LOBF, color = colour1)
     plt.plot(x2a, LOBF2, color = colour2)
@@ -111,7 +112,7 @@ modelvstrue(x1a, y1a, y1b, title="CPEH DISK FirstOvertone Modeldist/Truedist %",
 
 ###
 
-### Percentage Accuracy vs Distance Graphs ###
+### Percentage Error vs Distance Graphs ###
 
 ## Delta Scuti Variables ##
 
@@ -121,7 +122,7 @@ x2a = FundamentalblgdsF['Distance']
 y2a = (FundamentalblgdsF['SMCmodeldist']/FundamentalblgdsF['Distance'])*100
 y2b = (FundamentalblgdsF['LMCmodeldist']/FundamentalblgdsF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="DS BLG Fundamental Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="DS BLG Fundamental Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # Fundamental DISK #
 
@@ -129,7 +130,7 @@ x2a = FundamentaldiskdsF['Distance']
 y2a = (FundamentaldiskdsF['SMCmodeldist']/FundamentaldiskdsF['Distance'])*100
 y2b = (FundamentaldiskdsF['LMCmodeldist']/FundamentaldiskdsF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="DS DISK Fundamental Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="DS DISK Fundamental Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # FirstOvertone BLG #
 
@@ -137,7 +138,7 @@ x2a = FirstOvertoneblgdsF['Distance']
 y2a = (FirstOvertoneblgdsF['SMCmodeldist']/FirstOvertoneblgdsF['Distance'])*100
 y2b = (FirstOvertoneblgdsF['LMCmodeldist']/FirstOvertoneblgdsF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="DS BLG FirstOvertone Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="DS BLG FirstOvertone Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # FirstOvertone DISK #
 
@@ -145,7 +146,7 @@ x2a = FirstOvertonediskdsF['Distance']
 y2a = (FirstOvertonediskdsF['SMCmodeldist']/FirstOvertonediskdsF['Distance'])*100
 y2b = (FirstOvertonediskdsF['LMCmodeldist']/FirstOvertonediskdsF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="DS DISK FirstOvertone Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="DS DISK FirstOvertone Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 ## Classical Cepheid Variables ##
 
@@ -155,7 +156,7 @@ x2a = FundamentalblgcephF['Distance']
 y2a = (FundamentalblgcephF['SMCmodeldist']/FundamentalblgcephF['Distance'])*100
 y2b = (FundamentalblgcephF['LMCmodeldist']/FundamentalblgcephF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="CEPH BLG Fundamental Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="CEPH BLG Fundamental Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # Fundamental DISK #
 
@@ -163,7 +164,7 @@ x2a = FundamentaldiskcephF['Distance']
 y2a = (FundamentaldiskcephF['SMCmodeldist']/FundamentaldiskcephF['Distance'])*100
 y2b = (FundamentaldiskcephF['LMCmodeldist']/FundamentaldiskcephF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="CEPH DISK Fundamental Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="CEPH DISK Fundamental Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # FirstOvertone BLG #
 
@@ -171,7 +172,7 @@ x2a = FirstOvertoneblgcephF['Distance']
 y2a = (FirstOvertoneblgcephF['SMCmodeldist']/FirstOvertoneblgcephF['Distance'])*100
 y2b = (FirstOvertoneblgcephF['LMCmodeldist']/FirstOvertoneblgcephF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="CEPH BLG FirstOvertone Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="CEPH BLG FirstOvertone Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 # FirstOvertone DISK #
 
@@ -179,9 +180,12 @@ x2a = FirstOvertonediskcephF['Distance']
 y2a = (FirstOvertonediskcephF['SMCmodeldist']/FirstOvertonediskcephF['Distance'])*100
 y2b = (FirstOvertonediskcephF['LMCmodeldist']/FirstOvertonediskcephF['Distance'])*100
 
-accfunction(x2a, y2a, y2b, title="CEPH DISK FirstOvertone Percentage Accuracy % Vs True Distance", xlabel="True Distance", ylabel="Accuracy% (Modeldist/Truedist)")
+errorfunction(x2a, y2a, y2b, title="CEPH DISK FirstOvertone Percentage Error % Vs True Distance", xlabel="True Distance", ylabel="Error% (Modeldist/Truedist)")
 
 ###
+
+
+
 
 
 
